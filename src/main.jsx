3a8691fa43ce7636fea4455e1d28fe8507d6e150
annieherieau <annie.herieau@gmail.com>
2024-05-16 19:44:45 +0200

    fix basename

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import store from "./app/store.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/thp_mini-malt">
         <App />
       </BrowserRouter>
     </Provider>
